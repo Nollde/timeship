d9e1a7f994cd89491fce3be1145f60d2e4788df9
Dennis Daniel Nick Noll <dennis.noll@rwth-aachen.de>
2019-08-26 10:21:31 +0200

    [setup] updated version and requirements + now excludes testing

--- setup.py
+++ setup.py
@@ -5,16 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="timeship",
-    version="0.0.2",
+    version="1.0.1",
     author="Nollde",
     description="Easy timing of all kinds of python code.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://git.rwth-aachen.de/dennis.noll/timeship.git",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['test.py', ]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+       'numpy',
+   ]
 )
